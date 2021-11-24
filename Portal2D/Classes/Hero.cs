c5fc08c54e190de9f48a835bf523684531cedb2b
Jef <jef.v.d.a@live.be>
2021-11-24 15:32:46 +0100

    Fixed bugs

--- Portal2D/Classes/Hero.cs
+++ Portal2D/Classes/Hero.cs
@@ -9,35 +9,29 @@ using System.Text;
 
 namespace Portal2D.Classes
 {
-    class Hero : IGameObject,IMovable
+    class Hero : IGameObject, IMovable
     {
         private Texture2D texture;
-<<<<<<< HEAD
-        private Vector2 position ;
-=======
-        private MovementManager movementManager;
-        public Vector2 position { get; set; }
->>>>>>> main
-
-        public IInputReader inputReader { get; set; }
+        public Vector2 Position { get; set; }
+        public IInputReader InputReader { get; set; }
 
         Animation animation;
 
         public Hero(Texture2D texture, IInputReader inputReader)
         {
             this.texture = texture;
-            this.inputReader = inputReader;
+            this.InputReader = inputReader;
 
             animation = new Animation();
             animation.GetFramesFromTextureProperties(texture.Width, texture.Height, 6, 1);
 
-            position = new Vector2(0, 100);
+            Position = new Vector2(0, 100);
             //speed = new Vector2(0, 0);
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(texture, position, animation.CurrentFrame.SourceRectangle, Color.White);
+            spriteBatch.Draw(texture, Position, animation.CurrentFrame.SourceRectangle, Color.White);
         }
 
         public void Update(GameTime gameTime)
