5aae8bb63de5a7fc51decd43e436aca6cb290502
Jef <jef.v.d.a@live.be>
2021-12-22 13:18:10 +0100

    Auto stash before merge of "main" and "origin/main"

---
+++ Portal2D/Classes/Level/Level.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Portal2D.Classes.Level
+{
+    public class Level
+    {
+        private Texture2D backGround;
+
+        public Level(Texture2D backGround)
+        {
+            this.backGround = backGround;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(backGround, new Vector2(0, 0), Color.White);
+        }
+    }
+}
