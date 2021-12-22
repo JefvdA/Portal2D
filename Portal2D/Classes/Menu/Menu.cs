c39f5c7b490497793aacabfe76692f52362a75a8
Stef Santens <stef.santens@gmail.com>
2021-12-22 13:19:42 +0100

    Updated Menu

--- Portal2D/Classes/Menu/Menu.cs
+++ Portal2D/Classes/Menu/Menu.cs
@@ -9,16 +9,14 @@ namespace Portal2D.Classes.Menu
     class Menu
     {
         private Texture2D background;
-        private Vector2 position;
 
         public Menu(Texture2D Background) 
         {
             this.background = Background;
-            position = new Vector2(0, 0);
         }
         public void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(background, position, Color.White);
+            spriteBatch.Draw(background, new Vector2(0,0), Color.White);
         }
     }
 }
